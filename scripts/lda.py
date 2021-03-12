b062fcb0b3584cf842c0d0207ec1c9a6e304e904
unknown <caloja@espol.edu.ec>
2021-03-11 23:06:57 -0500

    Add lda incorporation

---
+++ scripts/lda.py
@@ -0,0 +1,206 @@
+import csv
+import os
+import pickle
+import re
+
+from textblob import TextBlob
+import numpy as np
+import nltk
+import pandas as pd
+from wordcloud import WordCloud
+from nltk.corpus import stopwords
+import gensim
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel# Compute Coherence Score
+from nltk.corpus import stopwords
+import spacy# Remove Stop Words
+import gensim.corpora as corpora# Create Dictionary
+import numpy as np
+import tqdm
+import pyLDAvis.gensim
+import pickle 
+import pyLDAvis# Visualize the topics
+
+import spacy 
+spacy.cli.download("en_core_web_sm")
+PATH = '../../files/'
+df_resena = pd.read_csv(
+    '{}reseña.csv'.format(PATH),
+    sep=',',
+#     index_col=0, 
+    error_bad_lines=False
+)
+
+df_resena_text = df_resena[['reseña_id', 'texto']]
+df_resena_text.columns = ['review_id', 'text']
+
+df_resena_text['text_processed'] = df_resena_text['text'].str.replace(r'[^\w\s]','').str.lower().copy()
+
+long_string = ' '.join(list(df_resena_text['text_processed'].dropna().values))
+
+stop_words = stopwords.words('english')
+
+wordcloud = WordCloud(
+                background_color="white", 
+                max_words=100, 
+                contour_width=3, 
+                contour_color='steelblue',
+                stopwords=stop_words
+)
+wordcloud.generate(long_string[:int(size/4)])
+
+
+
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
+
+data = df_resena_text['text_processed'].values.tolist()
+data_words = list(sent_to_words(data))
+
+stop_words = stopwords.words('english')
+stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
+
+# Define functions for stopwords, bigrams, trigrams and lemmatization
+def remove_stopwords(texts):
+    return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+
+def make_bigrams(texts):
+    return [bigram_mod[doc] for doc in texts]
+
+def make_trigrams(texts):
+    return [trigram_mod[bigram_mod[doc]] for doc in texts]
+
+def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+    """https://spacy.io/api/annotation"""
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent)) 
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    return texts_out
+
+
+
+
+# Remove Stop Words
+data_words_nostops = remove_stopwords(data_words)
+# Form Bigrams
+data_words_bigrams = make_bigrams(data_words_nostops)
+# Initialize spacy 'en' model, keeping only tagger component (for efficiency)
+nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
+# Do lemmatization keeping only noun, adj, vb, adv
+data_lemmatized = lemmatization(data_words_bigrams, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
+
+
+
+
+id2word = corpora.Dictionary(data_lemmatized)# Create Corpus
+texts = data_lemmatized# Term Document Frequency
+corpus = [id2word.doc2bow(text) for text in texts]# View
+print(corpus[:1])
+
+
+
+# Build LDA model
+lda_model = gensim.models.LdaMulticore(corpus=corpus,
+                                    id2word=id2word,
+                                    num_topics=10, 
+                                    random_state=100,
+                                    chunksize=100,
+                                    passes=10,
+                                    per_word_topics=True)
+
+
+coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+coherence_lda = coherence_model_lda.get_coherence()
+print('\nCoherence Score: ', coherence_lda)
+
+def compute_coherence_values(corpus, dictionary, k, a, b):
+    
+    lda_model = gensim.models.LdaMulticore(corpus=corpus,
+                                        id2word=dictionary,
+                                        num_topics=k, 
+                                        random_state=100,
+                                        chunksize=100,
+                                        passes=10,
+                                        alpha=a,
+                                        eta=b)
+    
+    coherence_model_lda = CoherenceModel(model=lda_model, texts=data_lemmatized, dictionary=id2word, coherence='c_v')
+    
+    return coherence_model_lda.get_coherence()
+
+
+
+
+grid = {}
+grid['Validation_Set'] = {}
+
+# Topics range
+min_topics = 2
+max_topics = 11
+step_size = 1
+topics_range = range(min_topics, max_topics, step_size)
+
+# Alpha parameter
+alpha = list(np.arange(0.01, 1, 0.3))
+alpha.append('symmetric')
+alpha.append('asymmetric')
+
+# Beta parameter
+beta = list(np.arange(0.01, 1, 0.3))
+beta.append('symmetric')
+
+# Validation sets
+num_of_docs = len(corpus)
+corpus_sets = [gensim.utils.ClippedCorpus(corpus, int(num_of_docs*0.75)), 
+               corpus]
+
+corpus_title = ['75% Corpus', '100% Corpus']
+
+model_results = {'Validation_Set': [],
+                 'Topics': [],
+                 'Alpha': [],
+                 'Beta': [],
+                 'Coherence': []
+                }
+
+# Can take a long time to run
+if 1 == 1:
+    pbar = tqdm.tqdm(total=(len(beta)*len(alpha)*len(topics_range)*len(corpus_title)))
+    # iterate through validation corpuses
+    for i in range(len(corpus_sets)):
+        # iterate through number of topics
+        for k in topics_range:
+            # iterate through alpha values
+            for a in alpha:
+                # iterare through beta values
+                for b in beta:
+                    # get the coherence score for the given parameters
+                    cv = compute_coherence_values(corpus=corpus_sets[i], dictionary=id2word, 
+                                                  k=k, a=a, b=b)
+                    # Save the model results
+                    model_results['Validation_Set'].append(corpus_title[i])
+                    model_results['Topics'].append(k)
+                    model_results['Alpha'].append(a)
+                    model_results['Beta'].append(b)
+                    model_results['Coherence'].append(cv)
+                    
+                    pbar.update(1)
+    pd.DataFrame(model_results).to_csv('./results/lda_tuning_results.csv', index=False)
+    pbar.close()
+
+
+lda_model = gensim.models.LdaMulticore(corpus=corpus,
+                                        id2word=id2word,
+                                        num_topics=8, 
+                                        random_state=100,
+                                        chunksize=100,
+                                        passes=10,
+                                        alpha=0.01,
+                                        eta=0.9)
+
+
+pyLDAvis.enable_notebook()
+LDAvis_prepared = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
+LDAvis_prepared
